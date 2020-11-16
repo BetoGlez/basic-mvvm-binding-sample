e799740f961881d224ff749cf04a43dc7c8e530b
alberto.gonzalezh@alumnos.upm.es <alberto.gonzalezh@alumnos.upm.es>
2020-11-16 19:41:37 +0100

    Basic binding excercises sample

---
+++ BindingExcercises/ViewModel/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace BindingExcercises.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DispatcherTimer timer;
+        private String currentTime = DateTime.Now.ToString("h:mm:ss tt");
+
+        public string CurrentTime
+        {
+            get
+            {
+                return currentTime;
+            }
+            set
+            {
+                currentTime = value;
+                OnPropertyChanged("CurrentTime");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public MainViewModel()
+        {
+            StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += new EventHandler<object>(timer_Tick);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, object e)
+        {
+            CurrentTime = DateTime.Now.ToString("h:mm:ss tt");
+        }
+
+    }
+}
